977c6a4fd64d4df93b2540eef91f8790c259fc6b
Johan Stuyts <j.stuyts@javathinker.com>
2026-05-29 23:14:00 +0200

    Make JSHint happy.

--- wicket-core/src/main/java/org/apache/wicket/ajax/res/js/wicket-ajax-jquery.js
+++ wicket-core/src/main/java/org/apache/wicket/ajax/res/js/wicket-ajax-jquery.js
@@ -145,35 +145,37 @@
 
 	// Copied from https://github.com/epoberezkin/fast-deep-equal 3.1.3
 	var equal = function (a, b) {
-		if (a === b) return true;
+		if (a === b) { return true; }
 
-		if (a && b && typeof a == 'object' && typeof b == 'object') {
-			if (a.constructor !== b.constructor) return false;
+		if (a && b && typeof a === 'object' && typeof b === 'object') {
+			if (a.constructor !== b.constructor) { return false; }
 
 			var length, i, keys;
 			if (Array.isArray(a)) {
 				length = a.length;
-				if (length != b.length) return false;
-				for (i = length; i-- !== 0;)
-					if (!equal(a[i], b[i])) return false;
+				if (length !== b.length) { return false; }
+				for (i = length; i-- !== 0;) {
+					if (!equal(a[i], b[i])) { return false; }
+				}
 				return true;
 			}
 
-			if (a.constructor === RegExp) return a.source === b.source && a.flags === b.flags;
-			if (a.valueOf !== Object.prototype.valueOf) return a.valueOf() === b.valueOf();
-			if (a.toString !== Object.prototype.toString) return a.toString() === b.toString();
+			if (a.constructor === RegExp) { return a.source === b.source && a.flags === b.flags; }
+			if (a.valueOf !== Object.prototype.valueOf) { return a.valueOf() === b.valueOf(); }
+			if (a.toString !== Object.prototype.toString) { return a.toString() === b.toString(); }
 
 			keys = Object.keys(a);
 			length = keys.length;
-			if (length !== Object.keys(b).length) return false;
+			if (length !== Object.keys(b).length) { return false; }
 
-			for (i = length; i-- !== 0;)
-				if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false;
+			for (i = length; i-- !== 0;) {
+				if (!Object.prototype.hasOwnProperty.call(b, keys[i])) { return false; }
+			}
 
 			for (i = length; i-- !== 0;) {
 				var key = keys[i];
 
-				if (!equal(a[key], b[key])) return false;
+				if (!equal(a[key], b[key])) { return false; }
 			}
 
 			return true;
@@ -718,11 +720,11 @@
 			}
 
 			if (attrs.bdi) {
-				var element = (attrs.bddc && !jQuery.isWindow(attrs.bddc))
-					? Wicket.$(attrs.bddc)
-					: ((attrs.c && !jQuery.isWindow(attrs.c))
-						? Wicket.$(attrs.c)
-						: null);
+				var element = (attrs.bddc && !jQuery.isWindow(attrs.bddc)) ?
+					Wicket.$(attrs.bddc) :
+					((attrs.c && !jQuery.isWindow(attrs.c)) ?
+						Wicket.$(attrs.c) :
+						null);
 				if (element) {
 					var blockDuplicatesPropertyName = "wicketAjaxBlockDuplicates" + attrs.bdi;
 					var previouslySubmittedData = element[blockDuplicatesPropertyName];
